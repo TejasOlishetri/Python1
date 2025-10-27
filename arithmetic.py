6f208b7076335ec157ebef4bb9b5f17611c2ebe5
Tejas Olishetri <olitejas2@gmail.com>
2025-10-28 01:06:45 +0530

    My First Program

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+print("Addition:", sum)
+sub = num1 - num2
+print("Subtraction:", sub)
+mul = num1 * num2
+print("Multiplication:", mul)
+div = float(num1) / float(num2)
+print("Division:", div)
